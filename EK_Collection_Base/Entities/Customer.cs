2517024e165805d14af2d87e3ca28fc1caa593aa
Edvinas K <edvinas.kesminas@gmail.com>
2018-11-29 23:05:23 +0200

    Created new entity placeholders

---
+++ EK_Collection_Base/Entities/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EK_Collection_Base.Entities
+{
+    public class Customer
+    {
+        public Customer() : this(Guid.NewGuid().ToString())
+        {
+
+        }
+
+        public Customer(string Id)
+        {
+            this.Id = Id;
+        }
+
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public ICollection<Address> Addresses { get; set; }
+    }
+}
